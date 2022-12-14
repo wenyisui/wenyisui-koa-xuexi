50fde92ee6dfcf9eb4e21274b499b3fbefbb6023
wenbin <1969138070@qq.com>
2022-12-14 19:55:51 +0800

    05-目录结构优化

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,13 +1,12 @@
 const Router=require('koa-router');
 
+const {register,login}=require('../controller/user.controller')
+
 const router=new Router({prefix:'/users'});
 
-
-
-// GET /users/
-router.get('/',(ctx,body)=>{
-    ctx.body='hello users'
-})
-
+//注册接口
+router.post('/register',register)
+//登录接口
+router.post('/login',login);
 
 module.exports=router;
