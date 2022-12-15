8b49187d57c58b9da0d74e89c2da51b3aec89c21
wenbin <1969138070@qq.com>
2022-12-15 18:01:13 +0800

    15-修改密码

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -2,7 +2,7 @@ const Router = require('koa-router');
 
 const { useValidator, verifyUser, crpyPassword, verifyLogin } = require('../middleware/user.middleware')
 const { auth }=require('../middleware/auth.middleware')
-const { register, login } = require('../controller/user.controller')
+const { register, login ,changePassword} = require('../controller/user.controller')
 
 const router = new Router({ prefix: '/users' });
 
@@ -11,9 +11,6 @@ router.post('/register', useValidator, verifyUser, crpyPassword, register)
 //登录接口
 router.post('/login', useValidator, verifyLogin, login);
 //修改密码接口
-router.patch('/',auth, (ctx, next) => {
-   console.log(ctx.state.user);
-   ctx.body = '修改密码成功'
-})
+router.patch('/', auth, crpyPassword,changePassword);
 
 module.exports = router;
